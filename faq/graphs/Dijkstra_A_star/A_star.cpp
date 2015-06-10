853223256d61ca904bd9821582200ce792766cf4
pamd <dongbo_hu@yahoo.com>
2015-06-10 16:04:56 -0400

    Update A_star.cpp

--- faq/graphs/Dijkstra_A_star/A_star.cpp
+++ faq/graphs/Dijkstra_A_star/A_star.cpp
@@ -1,28 +1,29 @@
+//
 // An overly simple A* search example, which uses the distance
 // from current position to the exit as the priority in priority queue.
 //
-// Base on B. Green "Programming Problems" Volume 1, pages 130-
-// https://github.com/ProgrammingProblems/Volume1/blob/master/graphical_search/maze.cpp
-// https://github.com/ProgrammingProblems/Volume1/blob/master/graphical_search/main.cpp
-// https://github.com/ProgrammingProblems/Volume1/blob/master/graphical_search/distance_heuristic.cpp
-
-#include <priority_queue>
+#include <queue>
 #include <iostream>
 #include <vector>
 #include <map>
 #include <list>
+#include <utility>
+#include <cstdlib>
+#include <cassert>
 
 using namespace std;
 
-const vector<pair<size_t, size_t> > offsets = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
+typedef pair<int, int> Position;
+vector<Position> offsets;
 
-typedef pair<size_t, size_t> Postion;
+typedef vector<string> GameBoard;
+GameBoard board;
 
 // Return Manhattan distance between two positions in the maze.
 // Based on:
 // https://github.com/ProgrammingProblems/Volume1/blob/master/graphical_search/distance_heuristic.cpp
-size_t distance(const Position begin, const Position end) {
-   return abs(begin.first - end.first) + abs(begin.second - end.second);
+size_t manhattan(const Position& p1, const Position& p2) {
+   return abs(p1.first - p2.first) + abs(p2.second - p2.second);
 }
 
 // Is the input position valid? ('.' is used to denote a valid position.)
@@ -32,62 +33,161 @@ bool passable(const Position& pos) {
   return board[pos.first][pos.second] == '.';
 }
 
-// A* search algorithm which uses the distance from current position
+// A* search algorithm that uses the distance from current position
 // to the exit as the priority in priority queue.
 // Based on:
 // https://github.com/ProgrammingProblems/Volume1/blob/master/graphical_search/maze.cpp
-void A_star(const GameBoard& maze, const Position& start, const Position& exit, list<Position>* route)
+void A_star(const GameBoard& maze, const Position& source, const Position& exit, list<Position>* route)
 {
    map<Position, Position> parent; // Key: position in a maze; Value: position from which key comes.
-
    priority_queue<pair<size_t, Position>, vector<pair<size_t, Position> >, greater<pair<size_t, Position> > > pq;
    // pair::size_t:   distance from current position to exit;
    // pair::Position: current position.
 
-   pq.push({distance(start, exit), start});
-   parent[start] = start;
+   pq.push(make_pair(manhattan(source, exit), source));
+   parent[source] = source;
    while (!pq.empty()) {
-      auto current = pq.top().second;
-
-      // If currenty position is the exit, populate the route, then return.
-      if (exit == current) {
+      Position current = pq.top().second;
+      if (exit == current) { // If currenty position is the exit, populate the route, then return.
          do {
             route->push_front(current);
             current = parent[current];
-         } while (current != start);
-         route->push_front(start);
+         } while (current != source);
+         route->push_front(source);
          return;
       }
 
       // If current position is not the exit, pop it out of pq,
       // then push its unvisited neighbors into pq.
       pq.pop();
-      for (auto offset : offsets) {
-         auto neighbor = Position(current.first + offset.first, current.second + offset.second);
+      cout << "Pop out (" << current.first << ", " << current.second << ")" << endl; // For debug only
+
+      for (size_t i = 0; i < offsets.size(); ++i) {
+         Position neighbor = Position(current.first + offsets[i].first, current.second + offsets[i].second);
          if (neighbor.first < maze.size() && neighbor.second < maze[neighbor.first].size()
             // Make sure neighbor position is not out of the maze
             && parent.end() == parent.find(neighbor) // Make sure neighbor has not been visited.
             && passable(neighbor)) {    // Make sure neighbor is a valid position.
             parent[neighbor] = current; // Memorize the position that the neighbor comes from.
-            pq.push({distance(neighbor, exit), neighbor});
+            pq.push(make_pair(manhattan(neighbor, exit), neighbor));
             // Note that the DISTANCE FROM NEIGHBOR TO THE EXIT is used as the priority.
          }
       } // end of "for"
    } // end of "while"
 }
 
+void print_route(const list<Position>& route) {
+   if (route.size()) {
+      for (list<Position>::const_iterator it = route.begin(); it != route.end(); ++it) {
+         cout << "(" << it->first << ", " << it->second << ")" << endl;
+      }
+   }
+   else {
+      cout << "None" << endl;
+   }
+   cout << endl;
+}
+
 // Test harness
 int main() {
-  list<Position> route;
-  board = { {'.', '.'}, {'.', '.'} };
-  A_star(board, {0, 0}, {1, 1}, &route);
-  ASSERT(!route.empty());
-  ASSERT(route.size() == 3);
+   offsets.push_back(make_pair(-1, 0));
+   offsets.push_back(make_pair(1, 0));
+   offsets.push_back(make_pair(0, -1));
+   offsets.push_back(make_pair(0, 1));
+   // make_pair(1, 0), make_pair(0, -1), make_pair(0, 1) };
 
-  route.clear();
-  board = { {'.', '*'}, {'*', '.'} };
-  maze(board, {0, 0}, {1, 1}, &route);
-  ASSERT(route.empty());
+   list<Position> route;
 
-  return 0;
+   /*
+   // Test #1:
+   board.push_back("..");
+   board.push_back("..");
+
+   Position s1(0, 0);
+   Position g1(1, 1);
+   A_star(board, s1, g1, &route);
+   cout << "Route in Test #1: " << endl;
+   print_route(route);
+
+   // Test #2:
+   route.clear();
+   board.clear();
+   board.push_back(".x");
+   board.push_back("x.");
+   Position s2(0, 0);
+   Position g2(1, 1);
+   A_star(board, s2, g2, &route);
+   cout << "Route in Test #2: " << endl;
+   print_route(route);
+
+   // Test #3:
+   route.clear();
+   board.clear();
+   board.push_back("....");
+   board.push_back("....");
+   board.push_back(".xxx");
+   board.push_back("....");
+   Position s3(0, 0);
+   Position g3(3, 3);
+   A_star(board, s3, g3, &route);
+   cout << "Route in Test #3: " << endl;
+   print_route(route);
+   */
+
+   // Test #4:
+   route.clear();
+   board.clear();
+   board.push_back(".....");
+   board.push_back(".x...");
+   board.push_back(".x.x.");
+   board.push_back("...x.");
+   Position s4(0, 0);
+   Position g4(3, 4);
+   A_star(board, s4, g4, &route);
+   cout << "\nRoute in Test #4: " << endl;
+   print_route(route);
+   //
+   // Test #4 shows that this algorithm may not give us the shortest path.
+   // The graph in this test is:
+   //
+   //                 s . . . .
+   //                 . x . . .
+   //                 . x . x .
+   //                 . . . x t
+   //
+   // s is the source node, t is goal, '.' means valid position, 'x' mean invalid position.
+   // The path found by the algorithm includes 12 nodes:
+   //
+   //                 (0, 0)
+   //                 (1, 0)
+   //                 (2, 0)
+   //                 (3, 0)
+   //                 (3, 1)
+   //                 (3, 2)
+   //                 (2, 2)
+   //                 (1, 2)
+   //                 (1, 3)
+   //                 (1, 4)
+   //                 (2, 4)
+   //                 (3, 4)
+   //
+   // But the shortest path includes only 8 nodes:
+   //
+   //                 (0, 0)
+   //                 (0, 1)
+   //                 (0, 2)
+   //                 (0, 3)
+   //                 (0, 4)
+   //                 (1, 4)
+   //                 (2, 4)
+   //                 (3, 4)
+   //
+   // The reason is because the priority key in pq is the Mahattan distance from current node to the goal,
+   // so some of the nodes in optimal path have lower priority than the nodes in sub-optimal path.
+   //
+   // A correct implementation should use the SUM of distance from source node to the current node AND
+   // the Mahattan distance from current node to the goal.
+   //
+
+   return 0;
 }
