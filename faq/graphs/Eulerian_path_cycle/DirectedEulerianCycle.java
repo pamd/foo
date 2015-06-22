9c731ac84f43feb1f3c0caf86099a056f461007b
pamd <dongbo_hu@yahoo.com>
2015-06-22 10:44:04 -0400

    Update DirectedEulerianCycle.java

--- faq/graphs/Eulerian_path_cycle/DirectedEulerianCycle.java
+++ faq/graphs/Eulerian_path_cycle/DirectedEulerianCycle.java
@@ -34,132 +34,145 @@
  ************************************************************************************/
 
 public class DirectedEulerianCycle {
-    private Stack<Integer> tour = new Stack<Integer>();
-    private boolean isEulerian = true;
-    private Stack<Integer> stack = new Stack<Integer>();
-    private Queue<Integer>[] adj;
+   private Stack<Integer> tour = new Stack<Integer>();
+   private boolean isEulerian = true;
+   private Stack<Integer> stack = new Stack<Integer>();
+   private Queue<Integer>[] adj;
 
-    public DirectedEulerianCycle(Digraph G) {
-        // Create local copy of adjacency lists
-        adj = (Queue<Integer>[]) new Queue[G.V()];
-        for (int v = 0; v < G.V(); v++) {
-            adj[v] = new Queue<Integer>();
-            for (int w : G.adj(v))
-                adj[v].enqueue(w);
-        }
+   public DirectedEulerianCycle(Digraph G) {
+      // Create local copy of adjacency lists
+      adj = (Queue<Integer>[]) new Queue[G.V()];
+      for (int v = 0; v < G.V(); v++) {
+         adj[v] = new Queue<Integer>();
+         for (int w : G.adj(v)) {
+            adj[v].enqueue(w);
+         }
+      }
 
-        // Find Eulerian tour
-        stack.push(0);
-        while (!stack.isEmpty()) {
-            int s = stack.pop();
-            tour.push(s);
-            int v = s;
-	    // If the vertex that was just popped out of stack 
-	    // still has outbound edges, pop it back into the stack.  
-            while (!adj[v].isEmpty()) {
-                stack.push(v);
-                v = adj[v].dequeue();
-            }
+      // Find Eulerian tour (cycle)
+      stack.push(0);
+      while (!stack.isEmpty()) {
+         int s = stack.pop();
+         tour.push(s);
+         int v = s;
+         // If the vertex that was just popped out of stack still has outbound edges,
+         // push it back into the stack.
+         while (!adj[v].isEmpty()) {
+            stack.push(v);
+            v = adj[v].dequeue();
+         }
 
-            if (v != s) 
-		isEulerian = false;
-        }
+         if (v != s) {
+            isEulerian = false;
+         }
 
-        // Check if all edges have been used. 
-	// (If input graph is not connected, then some edges haven't 
-	// been used, so the graph must have no Eulerian cycle.) 
-        for (int v = 0; v < G.V(); v++) {
-            if (!adj[v].isEmpty()) 
-		isEulerian = false;
-	}
-    }
+      }
 
-    // Return Eulerian tour, or null if no such tour
-    public Iterable<Integer> tour() {
-        if (!isEulerian) 
-	    return null;
-        return tour;
-    }
+      // Check whether all edges have been visited.
+      // If the input graph is not connected, then at least one edge has not been visited,
+      // so the graph must NOT have Eulerian cycle.
+      for (int v = 0; v < G.V(); v++) {
+         if (!adj[v].isEmpty()) {
+            isEulerian = false;
+         }
+      }
+   }
 
-    // Does the digraph have an Eulerian tour?
-    public boolean isEulerian() {
-        return isEulerian;
-    }
+   // Return Eulerian tour, or null (if no such tour exists).
+   public Iterable<Integer> tour() {
+      if (!isEulerian) {
+         return null;
+      }
+      return tour;
+   }
+
+   // Does the digraph have an Eulerian tour?
+   public boolean isEulerian() {
+      return isEulerian;
+   }
 
     // Test harness
     public static void main(String[] args) {
-        int V = Integer.parseInt(args[0]);
-        int E = Integer.parseInt(args[1]);
+       int V = Integer.parseInt(args[0]);
+       int E = Integer.parseInt(args[1]);
 
-        // Random graph of V vertices and approximately E edges
-        // with indegree[v] = outdegree[v] for all vertices
-        Digraph G = new Digraph(V);
-        int[] indegree  = new int[V];
-        int[] outdegree = new int[V];
-        int deficit = 0;
-        for (int i = 0; i < E - deficit/2; i++) {
-            int v = StdRandom.uniform(V);
-            int w = StdRandom.uniform(V);
-            if (v == w) { 
-		i--; 
-		continue; 
-	    }
-            G.addEdge(v, w);
-            if (outdegree[v] >= indegree[v]) 
-		deficit++;
-            else                             
-		deficit--;
+       // Random graph of V vertices and approximately E edges
+       // with indegree[v] = outdegree[v] for all vertices
+       Digraph G = new Digraph(V);
+       int[] indegree  = new int[V];
+       int[] outdegree = new int[V];
+       int deficit = 0;
+       for (int i = 0; i < E - deficit/2; i++) {
+          int v = StdRandom.uniform(V);
+          int w = StdRandom.uniform(V);
+          if (v == w) {
+             i--;
+             continue;
+          }
+          G.addEdge(v, w);
+          if (outdegree[v] >= indegree[v])
+             deficit++;
+          else
+             deficit--;
 
-            outdegree[v]++;
+          outdegree[v]++;
 
-            if (indegree[w] >= outdegree[w]) 
-		deficit++;
-            else                             
-		deficit--;
+          if (indegree[w] >= outdegree[w]) {
+             deficit++;
+          }
+          else {
+             deficit--;
+          }
 
-            indegree[w]++;
-        }
+          indegree[w]++;
+       }
 
-        while (deficit > 0) {
-            int v = StdRandom.uniform(V);
-            int w = StdRandom.uniform(V);
-            if (v == w) 
-		continue;
-            if (outdegree[v] >= indegree[v]) 
-		continue;
-            if (indegree[w]  >= outdegree[w]) 
-		continue;
+       while (deficit > 0) {
+          int v = StdRandom.uniform(V);
+          int w = StdRandom.uniform(V);
+          if (v == w) {
+             continue;
+          }
+          if (outdegree[v] >= indegree[v]) {
+             continue;
+          }
+          if (indegree[w]  >= outdegree[w]) {
+             continue;
+          }
 
-            G.addEdge(v, w);
+          G.addEdge(v, w);
 
-            if (outdegree[v] >= indegree[v]) 
-		deficit++;
-            else  
-		deficit--;
+          if (outdegree[v] >= indegree[v]) {
+             deficit++;
+          }
+          else {
+             deficit--;
+          }
 
-            outdegree[v]++;
+          outdegree[v]++;
 
-            if (indegree[w] >= outdegree[w]) 
-		deficit++;
-            else
-		deficit--;
+          if (indegree[w] >= outdegree[w]) {
+             deficit++;
+          }
+          else {
+             deficit--;
+          }
 
-            indegree[w]++;
-        }
+          indegree[w]++;
+       }
 
-        StdOut.println(G);
+       StdOut.println(G);
 
-        DirectedEulerianCycle euler = new DirectedEulerianCycle(G);
-        if (euler.isEulerian()) {
-            for (int v : euler.tour()) {
-                StdOut.print(v + " ");
-            }
-            StdOut.println();
-        }
-        else {
-            StdOut.println("Not eulerian");
-        }
+       DirectedEulerianCycle euler = new DirectedEulerianCycle(G);
+       if (euler.isEulerian()) {
+          for (int v : euler.tour()) {
+             StdOut.print(v + " ");
+          }
+          StdOut.println();
+       }
+       else {
+          StdOut.println("Not eulerian");
+       }
     }
 
 }
-
